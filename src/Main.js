039cc046eb437f182bf2b5f7c03ce7a811a1de39
Dasarathi Rout <dasarathi.mca@gmail.com>
2020-04-17 13:09:47 +0530

    LetsReact JavaScript Apllication Initial Commit.

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="main-header">
+        <p>
+          <div>Lets React To Main.JS !!!</div>
+        </p>
+      </header>
+    </div>
+    //     React.createElement('div',{className:App},"Lets React To Main.JS !")
+  );
+}
+
+export default Main;
