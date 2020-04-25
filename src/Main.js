552333e4bf1e6660ec05a49cfaf190b51574f86b
Dasarathi Rout <DasarathiRout@live.com>
2020-04-25 08:54:00 +0530

    Uses Of ErrorBoundary

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './main.css';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 class Main extends Component {
 
   state = {
-    canBeShown:false
+    canBeShown: false
   }
-
+  
   render() {
+
     return (
       <div className='Main'>
         <hr className='main-hr' />
+        <ErrorBoundary>Error Boundary Works Fine.</ErrorBoundary>
       </div>
     );
   }
